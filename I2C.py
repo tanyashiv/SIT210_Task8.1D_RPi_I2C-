926a9b2d75c68c2f05c1edf8b90bf7d8280ef40b
tanyashiv <136065239+tanyashiv@users.noreply.github.com>
2023-10-22 00:58:57 +0530

    I2C.py

--- I2C.py
+++ I2C.py
@@ -1,64 +1,37 @@
-#Import necessary libraries
+# Import necessary libraries
+import smbus
+import time
 
-import time  # Use the time library to import sleep and time functions.
-import RPi.GPIO as GPIO     # Import the RPi.GPIO library for GPIO control.
+# Define I2C device address and resolution mode
+comp_Address =  0x23
+res_mode = 0x20
 
-# Set the GPIO mode to use BCM pin numbering.
-GPIO.setmode(GPIO.BCM)  
+# Initialize the I2C bus
+bus = smbus.SMBus(1)
 
+# Function to read light intensity value
+def light_val():
+     # Read a block of data from the I2C device
+    l = bus.read_i2c_block_data(comp_Address, res_mode)
+    return Convert_data(l) # Convert the data to a light intensity value
 
-# Define the GPIO pin numbers for the ultrasonic sensor-trig pin and echo pin, and the LED.
-LED_pin = 12
-trig_pin=4
-echo_pin=17
+# Function to convert data to light intensity value
+def Convert_data(data):
+    return ((data[1] + (256 * data[0]))/ 1.2)  # Combine two bytes of data to get the light intensity value
 
-
-# Set up the GPIO pins as either input or output.
-GPIO.setup(trig_pin, GPIO.OUT)
-GPIO.setup(echo_pin, GPIO.IN)
-GPIO.setup(LED_pin, GPIO.OUT)
-
-
-# Function to calculate distance using the ultrasonic sensor.
-def calc_distance():
-
-    # Produce a short pulse on the trigger pin.
-    GPIO.output(trig_pin, True)
-    time.sleep(0.00001)
-    GPIO.output(trig_pin, False)
-
-
-# Measure the time echo pin takes from low to high and back to low.
-    while GPIO.input(echo_pin) == 0:
-        pulse_start_time = time.time()
-    while GPIO.input(echo_pin) == 1:
-        pulse_end_time = time.time()
-
-
-# Determine the pulse duration and convert it into centimetres.
-    total_pulse_duration = pulse_end_time - pulse_start_time
-    distance= total_pulse_duration * 34300 / 2      #Speed of sound in cm/s
-    
-    return distance
-
-
-#Main code
-try:
-    while True:
-        dist_cm = calc_distance()       #Call the calc_distance function to get the distance in cms.
-        
-        if dist_cm < 10:                 # Check if the distance is less than 10 cm (object is in range).
-          
-            GPIO.output(LED_pin, GPIO.HIGH)      #LED will be on.
-            print("Object is in range")
-        else:            # Else the distance is more than 10 cm (object is out of range).
-            
-            GPIO.output(LED_pin, GPIO.LOW)       #LED will be off.
-            print("Object is out of range")
-        
-        time.sleep(0.15)   # Sleep for 0.15 second before taking the next distance.
-
-
-# Handle a keyboard interrupt to clear the GPIO pins (Ctrl+C).
-except KeyboardInterrupt:
-    GPIO.cleanup()
+# Continuous loop for reading and processing light intensity
+while True:
+     Intensity = light_val()  # Get the current light intensity value
+     time.sleep(1)   # Pause for 1 second
+     
+     # Determine the intensity level and print the corresponding message
+     if(Intensity > 2600):
+         print("Intensity of light: Too Bright")
+     elif(Intensity > 1600 and Intensity < 2600):
+         print("Intensity of light: Bright")
+     elif(Intensity > 1100 and Intensity < 1600):
+         print("Intensity of light: Medium")
+     elif(Intensity > 600 and Intensity < 1100):
+         print("Intensity of light: Dark")
+     elif(Intensity < 600):
+         print("Intensity of light: Too Dark")
